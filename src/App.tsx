667ff963f457db4889ad29f0b1c6a78128876f64
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-08-09 20:25:56 +0200

    Removing axios get from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import axios from 'axios'
-import logo from './logo.svg';
+import IssuesList from './components/IssuesList'
 import './App.scss';
-import GLOBAL from './global';
 
-type AppState = { 
-  issuesList: Array<Object>
-};
-
-class App extends React.Component< any, AppState > {
-  constructor(props: any){
-      super(props);
-      this.state = { issuesList: [] };
-  }
-
-  componentDidMount() {
-    this.getIssuesList(GLOBAL.issuesAPI);
-  }
-
-  async getIssuesList(url: string) {
-
-    const res = await axios.get(url);
-    const data : Array<Object> = await res.data;
-
-    this.setState({ issuesList: data })
-  }
-
+class App extends React.Component {
+  
   render() {
     return (
       <div className="App-wrapper">
@@ -41,7 +19,7 @@ class App extends React.Component< any, AppState > {
         </header>
 
         <section className="App-list">
-          
+          <IssuesList></IssuesList>
         </section>
       </div>
     );
