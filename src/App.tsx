81593f9414cc1872cf7b02cb5aec1ef13e5c80d2
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-08-09 18:51:51 +0200

    Making a call to issuesAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
+import GLOBAL from './global';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppState = { 
+  issuesList: Array<Object>
+};
+
+class App extends React.Component< any, AppState > {
+  constructor(props: any){
+      super(props);
+      this.state = { issuesList: [] };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+
+    const res = await axios.get(GLOBAL.issuesAPI);
+    const data : Array<Object> = await res.data;
+
+    this.setState({ issuesList: data })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p></p>
+          <p>
+            <code>
+              {JSON.stringify(this.state.issuesList)}
+            </code>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
