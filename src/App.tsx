1511b6542924b4fd1afc839ba9dc360799f48b2d
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-08-10 00:26:02 +0200

    Re-styling the app header

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ class App extends React.Component {
       <div className="App-wrapper">
 
         <header className="App-header">
-          <h2 className="App-logo">ds-gh-issues</h2>
+          <a className="App-logo" href="https://github.com/nerioslamaj/ds-gh-issues" target="_blank" rel="noopener noreferrer">
+            <h1>ds-gh-issues</h1>
+          </a>
 
           <p className="App-intro">A simple app that pulls the latest issues from Create React App repo</p>
         </header>
